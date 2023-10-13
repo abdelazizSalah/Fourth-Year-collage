252e5415c350920618d7a768b88c97afb811946f
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-10-13 14:53:54 +0300

    solving the palindrome successfully!

--- First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/palindrome_check.py
+++ First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/palindrome_check.py
@@ -1,10 +1,25 @@
 import utils
 
+
 def palindrome_check(string: str) -> bool:
     '''
     This function takes string and returns where a string is a palindrome or not
     A palindrome is a string that does not change if read from left to right or from right to left
     Assume that empty strings are palindromes
     '''
-    #TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
+
+    #! lower all characters
+    string = string.lower()
+    length: int = len(string)
+    for i in range(length // 2):
+        if string[i] != string[-1-i]:
+            return False
+    return True
+
+
+print(palindrome_check('BaaB'))
+print(palindrome_check('Apple'))
+print(palindrome_check('Radar'))
+print(palindrome_check('radar'))
+print(palindrome_check('Abdelaziz'))
+print(palindrome_check('ZozoZ'))
