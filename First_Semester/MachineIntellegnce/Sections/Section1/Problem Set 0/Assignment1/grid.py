a6b630dca977a4d25fb08cc7beceee8c5de2dabd
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-10-13 15:34:50 +0300

    solving locators in two different approaches

--- First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/grid.py
+++ First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/grid.py
@@ -3,20 +3,22 @@ from typing import Any, List, Tuple
 
 class Grid:
     # Th following line defines the data type for the instance variable "__data"
-    # This type hint means the "__data" is a list of lists containing any type of data 
-    __data : List[List[Any]]
+    # This type hint means the "__data" is a list of lists containing any type of data
+    __data: List[List[Any]]
 
     def __init__(self, width: int, height: int) -> None:
         self.__data = [[None]*width for _ in range(height)]
-    
+
     @property
     def width(self) -> int:
+        # ? number of columns
         return 0 if len(self.__data) == 0 else len(self.__data[0])
-    
+
     @property
     def height(self) -> int:
+        # ? number of rows
         return len(self.__data)
-    
+
     # The key used to access the grid is a tuple of two integers (x, y)
     def __getitem__(self, key: Tuple[int, int]) -> Any:
         x, y = key
@@ -25,7 +27,7 @@ class Grid:
             if 0 <= x < len(row):
                 return row[x]
         return None
-    
+
     # The key used to access the grid is a tuple of two integers (x, y)
     def __setitem__(self, key, value) -> None:
         x, y = key
@@ -33,12 +35,12 @@ class Grid:
             row = self.__data[y]
             if 0 <= x < len(row):
                 row[x] = value
-    
+
     # This function is called whenever we convert the grid into a string
     # This is useful for printing
     def __str__(self) -> str:
         return '\n'.join(' '.join(str(cell) for cell in row) for row in self.__data)
-    
+
     # This static method creates a grid from a list of lists
     @staticmethod
     def GridFromArray(array: List[List[Any]]) -> 'Grid':
--- First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/locator.py
+++ First_Semester/MachineIntellegnce/Sections/Section1/Problem Set 0/Assignment1/locator.py
@@ -2,12 +2,30 @@ from typing import Any, Set, Tuple
 from grid import Grid
 import utils
 
-def locate(grid: Grid, item: Any) -> Set[Tuple[int,int]]:
+
+def locate(grid: Grid, item: Any) -> Set[Tuple[int, int]]:
     '''
     This function takes a 2D grid and an item
     It should return a list of (x, y) coordinates that specify the locations that contain the given item
     To know how to use the Grid class, see the file "grid.py"  
     '''
-    #TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
-    
+
+    #! Algorithm: just iterate over the whole grid, and for each entry call getcell.
+    #! Then we need to check if the item is same as what we found, then we just need
+    #! to get add this indicies to our set result, otherwise we just keep iterating.
+    rows: int = grid.height
+    columns: int = grid.width
+
+    #! Using normal forloops
+    # setRes: Set[Tuple[int, int]] = set()
+    # for i in range(rows+1):
+    #     for j in range(columns+1):
+    #         if(grid.__getitem__((i, j)) == item):
+    #             print(grid.__getitem__((i, j)), item)
+    #             setRes.add((i, j))
+    #             print(setRes)
+
+    # ? Compact way of solving the problem :D
+    setRes: Set[Tuple[int, int]] = {(i, j) for i in range(
+        rows + 1) for j in range(columns + 1) if(grid.__getitem__((i, j)) == item)}
+    return setRes
