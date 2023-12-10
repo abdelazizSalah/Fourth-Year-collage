b83094b04a9aaee40db3b299f09eb91549d373b6
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-12-10 02:18:24 +0200

    finish register screen layout

---
+++ First_Semester/consultation/project/front_end_code/lib/data/user_data.dart
@@ -0,0 +1,15 @@
+import 'dart:isolate';
+
+class User {
+  String? userName; // must be unique
+  String? password;
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? birthDate;
+  String? gender;
+  String? city;
+  String? adress;
+  String? role; // manager / fan
+
+}
--- First_Semester/consultation/project/front_end_code/lib/screens/login_screen.dart
+++ First_Semester/consultation/project/front_end_code/lib/screens/login_screen.dart
@@ -10,13 +10,14 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
-  bool buildLogIn = true;
-  Widget buildLoginLayout(double textScaleFactor, GlobalKey outerKey) {
+  bool buildLogIn = false;
+  Widget buildLoginLayout(double textScaleFactor) {
     return Expanded(
-        flex: 6,
+        flex: 5,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
+            // margin: const EdgeInsets.only(top: 10),
             Text('Welcome Back!',
 
                 // it should be underlined
@@ -27,45 +28,49 @@ class _LogInScreenState extends State<LogInScreen> {
                     fontSize: 35 * textScaleFactor)),
 
             // input text field
-            TextField(
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'RubikBubbles',
-                  fontSize: 20 * textScaleFactor),
-              decoration: InputDecoration(
-                hintText: 'Username',
-                icon: Icon(Icons.person,
-                    color: Colors.amber, size: 35 * textScaleFactor),
-                hintStyle: const TextStyle(
-                    color: Colors.black, fontFamily: 'RubikBubbles'),
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              style: const TextStyle(
-                  color: Colors.black, fontFamily: 'RubikBubbles'),
-              decoration: InputDecoration(
-                hintText: 'Password',
-                hintStyle: TextStyle(
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextField(
+                style: TextStyle(
                     color: Colors.black,
                     fontFamily: 'RubikBubbles',
                     fontSize: 20 * textScaleFactor),
-                // change the inserted text style
-                counterStyle: const TextStyle(color: Colors.red),
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  icon: Icon(Icons.person,
+                      color: Colors.amber, size: 35 * textScaleFactor),
+                  hintStyle: const TextStyle(
+                      color: Colors.black, fontFamily: 'RubikBubbles'),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 5),
+              child: TextField(
+                obscureText: true,
+                style: const TextStyle(
+                    color: Colors.black, fontFamily: 'RubikBubbles'),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'RubikBubbles',
+                      fontSize: 20 * textScaleFactor),
+                  // change the inserted text style
+                  counterStyle: const TextStyle(color: Colors.red),
 
-                icon: Icon(
-                  Icons.password,
-                  color: Colors.amber,
-                  size: 35 * textScaleFactor,
+                  icon: Icon(
+                    Icons.password,
+                    color: Colors.amber,
+                    size: 35 * textScaleFactor,
+                  ),
                 ),
               ),
             ),
             Align(
               alignment: Alignment.centerRight,
               child: TextButton(
-                onPressed: () {
-                  print(outerKey.currentContext!.size!.width);
-                },
+                onPressed: () {},
                 child: Text(
                   'Forgot Password?',
                   style: TextStyle(
@@ -75,33 +80,290 @@ class _LogInScreenState extends State<LogInScreen> {
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.amber,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
-              ),
-              child: Text(
-                'Log In',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'RubikBubbles',
-                    fontSize: 20 * textScaleFactor),
+                child: Text(
+                  'Log In',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'RubikBubbles',
+                      fontSize: 20 * textScaleFactor),
+                ),
               ),
             ),
           ],
         ));
   }
 
-  Widget buildRegiesterLayout(double textScaleFactor) {
-    return const Card(
-      child: Center(child: Text('Hello')),
+  final newMemberData = [
+    [
+      ' userName',
+      Icons.person,
+    ], // must be unique], // must be unique
+    [
+      ' password',
+      Icons.password,
+    ], // must be unique],
+    [
+      ' email',
+      Icons.email,
+    ], // must be unique],
+    [
+      ' firstName',
+      Icons.abc,
+    ], // must be unique],
+    [
+      ' lastName',
+      Icons.abc_rounded,
+    ], // must be unique],
+    [
+      ' birthDate',
+      Icons.date_range,
+    ], // must be unique],
+    // [
+    //   ' gender',
+    //   Icons.male,
+    // ], // must be unique],
+    [
+      ' city',
+      Icons.cabin,
+    ], // must be unique],
+    [
+      ' adress',
+      Icons.cabin_rounded,
+    ], // must be unique],
+    // [
+    //   ' role',
+    //   Icons.radar_sharp,
+    // ], // must be unique], // manager / fan
+  ];
+  Widget buildDropBox(bool horizontal, double textScaleFactor) {
+    if (horizontal) {
+      return Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Wrap(children: [
+              Icon(Icons.male, color: Colors.amber, size: 35 * textScaleFactor),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: DropdownButton<String>(
+                  value: gender,
+                  dropdownColor: Colors.amber,
+                  icon: const Icon(Icons.no_sim_rounded),
+                  iconSize: 0,
+                  elevation: 16,
+                  alignment: Alignment.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'RubikBubbles',
+                      fontSize: 20 * textScaleFactor),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.amber,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      gender = newValue!;
+                    });
+                  },
+                  items: ['Male', 'Female']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ]),
+          ),
+          // drop down menu for the role
+          Expanded(
+            child: Wrap(
+              children: [
+                Icon(Icons.workspace_premium_rounded,
+                    color: Colors.amber, size: 35 * textScaleFactor),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: DropdownButton<String>(
+                    value: role,
+                    dropdownColor: Colors.amber,
+                    icon: const Icon(Icons.no_sim_rounded),
+                    iconSize: 0,
+                    elevation: 16,
+                    alignment: Alignment.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'RubikBubbles',
+                        fontSize: 20 * textScaleFactor),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.amber,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        role = newValue!;
+                      });
+                    },
+                    items: ['Fan', 'Manager']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            child: Row(children: [
+              Icon(Icons.male, color: Colors.amber, size: 35 * textScaleFactor),
+              SizedBox(
+                width: 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButton<String>(
+                    value: gender,
+                    dropdownColor: Colors.amber,
+                    icon: const Icon(Icons.no_sim_rounded),
+                    iconSize: 0,
+                    elevation: 16,
+                    alignment: Alignment.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'RubikBubbles',
+                        fontSize: 20 * textScaleFactor),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.amber,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        gender = newValue!;
+                      });
+                    },
+                    items: ['Male', 'Female']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          // drop down menu for the role
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Icon(Icons.workspace_premium_rounded,
+                  color: Colors.amber, size: 35 * textScaleFactor),
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButton<String>(
+                    value: role,
+                    dropdownColor: Colors.amber,
+                    icon: const Icon(Icons.no_sim_rounded),
+                    iconSize: 0,
+                    elevation: 16,
+                    alignment: Alignment.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'RubikBubbles',
+                        fontSize: 20 * textScaleFactor),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.amber,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        role = newValue!;
+                      });
+                    },
+                    items: ['Fan', 'Manager']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      );
+    }
+  }
+
+  String gender = 'Male';
+  String role = 'Fan';
+  Widget buildRegiesterLayout(double textScaleFactor, bool horizontal) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Text('Welcome to EPL Reservo!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.amber,
+                fontFamily: 'RubikBubbles',
+                fontSize: 28 * textScaleFactor)),
+
+        ...newMemberData
+            .map((element) => SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                        obscureText: element[0] == ' password',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'RubikBubbles',
+                            fontSize: 20 * textScaleFactor),
+                        decoration: InputDecoration(
+                          icon: Icon(element[1] as IconData,
+                              color: Colors.amber, size: 35 * textScaleFactor),
+                          hintText: element[0] as String,
+                          hintStyle: TextStyle(
+                              color: Colors.black87,
+                              fontFamily: 'RubikBubbles',
+                              fontSize: 20 * textScaleFactor),
+                          // change the inserted text style
+                          counterStyle: const TextStyle(color: Colors.red),
+                        )),
+                  ),
+                ))
+            .toList(),
+
+        // drop down menu for gender
+        buildDropBox(horizontal, textScaleFactor)
+      ],
     );
   }
 
-  Widget signUpRegisterLayout(double textScaleFactor) {
+  Widget topButtonsLayout(double textScaleFactor) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -142,15 +404,13 @@ class _LogInScreenState extends State<LogInScreen> {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final textScaleFactor = mediaQuery.textScaleFactor;
-    final outerContainerKey = GlobalKey();
-    bool minWidth = mediaQuery.size.width > 800;
+    bool minWidth = mediaQuery.size.width > 700;
 
-    bool minHeight = mediaQuery.size.height < 400;
+    bool minHeight = mediaQuery.size.height < 600;
 
     return Scaffold(
       appBar: const CustomAppBar(title: 'EPL Reservo'),
       body: Container(
-        key: outerContainerKey,
         width: mediaQuery.size.width,
         height: mediaQuery.size.height,
         decoration: const BoxDecoration(
@@ -162,43 +422,47 @@ class _LogInScreenState extends State<LogInScreen> {
             color: Colors.transparent),
         child: Align(
           alignment: const Alignment(0, -0.8),
-          heightFactor: 0.5,
+          heightFactor: 1,
           widthFactor: 0.5,
-          child: SizedBox(
-            width:
-                mediaQuery.size.width * (0.45 * (minWidth ? 1 : (.8 / 0.45))),
-            height: mediaQuery.size.height * (0.45 * (!minHeight ? 1 : 2)),
-            child: Card(
-              elevation: 5,
-              color: const Color.fromARGB(92, 255, 193, 7),
-              child: minHeight
-                  ? Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Increase the window height please!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.amber,
-                            fontSize: 30 * textScaleFactor,
-                            fontFamily: 'RubikBubbles'),
-                      ),
-                    )
-                  : Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: Column(
+          child: Card(
+            elevation: 5,
+            color: const Color.fromARGB(92, 255, 193, 7),
+            child: minHeight
+                ? Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Increase the window height please!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 30 * textScaleFactor,
+                          fontFamily: 'RubikBubbles'),
+                    ),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: SizedBox(
+                      width: mediaQuery.size.width *
+                          (0.3 * (minWidth ? 1 : (.9 / 0.45))),
+                      height: !buildLogIn
+                          ? mediaQuery.size.height
+                          : mediaQuery.size.height *
+                              (0.4 * (!minHeight ? 1 : 2)),
+                      child: ListView(
                         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        // mainAxisSize: MainAxisSize.min,
+                        // itemExtent: 50,
+
                         children: [
-                          signUpRegisterLayout(textScaleFactor),
+                          topButtonsLayout(textScaleFactor),
                           buildLogIn
-                              ? buildLoginLayout(
-                                  textScaleFactor, outerContainerKey)
-                              : buildRegiesterLayout(textScaleFactor)
+                              ? buildLoginLayout(textScaleFactor)
+                              : buildRegiesterLayout(textScaleFactor, minWidth)
                         ],
                       ),
                     ),
-            ),
+                  ),
           ),
         ),
       ),
